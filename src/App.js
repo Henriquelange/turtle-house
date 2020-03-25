fd79e6ec9cd0e8a363addb90a6ac2300579cdb88
Henrique Lange <hlangeseverino@gmail.com>
2020-03-24 22:07:40 -0300

    Add new material-ui template structure
    A nova estrutura usará os componentes do material-ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React oee
-        </a>
-      </header>
-    </div>
-  );
-}
+import Components from "./views/Components/Components.js";
+import LandingPage from "./views/LandingPage/LandingPage.js";
+import ProfilePage from "./views/ProfilePage/ProfilePage.js";
+import LoginPage from "./views/LoginPage/LoginPage.js";
+
+import "./assets/scss/material-kit-react.scss?v=1.8.0";
+
+var hist = createBrowserHistory();
+
+const App = () => (<Router history={hist}>
+    <Switch>
+      <Route path="/components" component={Components} />
+      <Route path="/profile" component={ProfilePage} />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/" component={LandingPage} />
+    </Switch>
+  </Router>
+)
 
 export default App;
