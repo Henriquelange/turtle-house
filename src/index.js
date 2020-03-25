d469b9affe0047f48093e253e28b5ac88554d8c4
Henrique Lange <hlangeseverino@gmail.com>
2020-03-24 22:17:53 -0300

    Change routes to be imported from App

--- src/index.js
+++ src/index.js
@@ -1,32 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
-
-import "./assets/scss/material-kit-react.scss?v=1.8.0";
-
-// pages for this product
-import Components from "./views/Components/Components.js";
-import LandingPage from "./views/LandingPage/LandingPage.js";
-import ProfilePage from "./views/ProfilePage/ProfilePage.js";
-import LoginPage from "./views/LoginPage/LoginPage.js";
-
-var hist = createBrowserHistory();
+import App from './App';
 
 ReactDOM.render(
-  // <App>
-    <Router history={hist}>
-      <Switch>
-        <Route path="/landing-page" component={LandingPage} />
-        <Route path="/profile-page" component={ProfilePage} />
-        <Route path="/login-page" component={LoginPage} />
-        <Route path="/" component={Components} />
-      </Switch>
-    </Router>,
-  // </App>,
+  <App />,
   document.getElementById("root")
 );
-
-
-// ReactDOM.render(<App />, document.getElementById('root'));
